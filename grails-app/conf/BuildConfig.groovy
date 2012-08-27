418b977f1f20971c288bdafaf8148b7b108fd2a5
Natalya G. Portnov <ngp@umn.edu>
2012-08-27 12:22:48 -0500

    Upgrading to grails 1.3.8. Removing custom version of spring-security-core

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,13 +32,8 @@ grails.project.dependency.resolution = {
 		compile ":hibernate:$grailsVersion"
 		compile ":tomcat:$grailsVersion"
 
-		//compile ":spring-security-core:1.2.7.3"
-		//compile ":spring-security-ldap:1.0.6"
-		//compile ":spring-security-shibboleth-native-sp:1.0.3"
-		//provided ":spring-security-mock:1.0.1"
-		
 		compile ":spring-security-core:1.2.7.3"
-		compile ":spring-security-ldap:1.0.5.1"
+		compile ":spring-security-ldap:1.0.6"
 		compile ":spring-security-shibboleth-native-sp:1.0.3"
 		provided ":spring-security-mock:1.0.1"
 		
